020e5a5d021748f9a9dec543dd46c2cd926fcec2
Edmilson Filho <edmilson.00000857761@unicap.br>
2026-03-02 17:59:17 -0300

    Criação da classe Filme, seus atributos e seu construtor

---
+++ Projeto/src/models/Filme.java
@@ -0,0 +1,20 @@
+public class Filme {
+
+    private String nome; 
+    private String duracao; 
+    private String sinopse; 
+    private int valorIngresso; 
+
+
+
+public Filme(String nome, String duracao, String sinopse, int valorIngresso){
+    this.nome = nome; 
+    this.duracao = duracao; 
+    this.sinopse = sinopse; 
+    this.valorIngresso = valorIngresso; 
+    }
+
+
+}
+
+

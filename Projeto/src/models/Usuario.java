7acd4ec5fc27a06a2327a91f88d4c89b5a5d5857
Edmilson Filho <edmilson.00000857761@unicap.br>
2026-03-02 17:44:35 -0300

    Criação do construtor da classe Usuario

---
+++ Projeto/src/models/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario{
+
+    private String user; 
+    private String cpf; 
+    private String senha; 
+    private String sexo; 
+    private String email; 
+    private String nomeCartao; 
+    private String numeroCartao; 
+    private String codigoCartao; 
+    private int idade; 
+
+    public Usuario(String user, String cpf, String senha, String sexo, String email, String nomeCartao, String numeroCartao, String codigoCartao, int idade){
+
+        this.user = user;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.email = email;
+        this.nomeCartao = nomeCartao;
+        this.numeroCartao = numeroCartao;
+        this.codigoCartao = codigoCartao;
+    }
+}
